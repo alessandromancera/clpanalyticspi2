d20fdf44acca8e4fd1760f59a598646ae22f3cdc
Alessandro Lima Mancera <alessandro.mancera@hotmail.com>
2022-05-24 13:16:41 +0000

    Update port

--- src/server.js
+++ src/server.js
@@ -8,10 +8,13 @@ require('./database/index');
 
 const app = express();
 
+// PORT process.env.APP_PORT
+const PORT = process.env.PORT || 8081;
+
 app.use(cors());
 app.use(express.json());
 
 app.use('/', Route);
 
 dotenv.config();
-app.listen(process.env.APP_PORT, () => console.log('Sproesser: Server Up and running in port',process.env.APP_PORT));
+app.listen(PORT, () => console.log('Sproesser: Server Up and running in port',PORT));

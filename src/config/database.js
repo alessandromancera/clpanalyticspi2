6b39b43a91d3df7ce6f9fd8f04118ec647fda546
Alessandro Lima Mancera <alessandro.mancera@hotmail.com>
2022-06-30 18:17:45 +0000

    Update database Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -4,10 +4,14 @@ dotenv.config();
 
 module.exports = {
     dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_NAME,
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    host: "us-cdbr-east-05.cleardb.net",
+    database: "heroku_b49f2fc3475df13",
+    username: "b0aaf76ab20c2a",
+    password: "e706d75b",
     port: process.env.DB_PORT,
     logging: false,
     //timezone: "-03:00",

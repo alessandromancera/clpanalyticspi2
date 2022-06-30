0f9ba53e17a8e69199f78708a87e9621d947c8b7
Alessandro Lima Mancera <alessandro.mancera@hotmail.com>
2022-06-30 18:40:29 +0000

    Update Port Heroku

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ const app = require('./config')
 const dotenv = require('dotenv');
 dotenv.config();
 
-const PORT = process.env.APP_PORT || 8081;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => console.log('Sproesser: Server Up and running in port',PORT));
